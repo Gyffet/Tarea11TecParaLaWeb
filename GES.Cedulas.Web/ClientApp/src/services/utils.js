f7beefa99fa90bd9b53815e98eb4cf198ac39ea5
oguerrerosqd <oguerrerosqd@outlook.com>
2022-06-03 13:34:52 -0500

    jala

--- GES.Cedulas.Web/ClientApp/src/services/utils.js
+++ GES.Cedulas.Web/ClientApp/src/services/utils.js
@@ -1,51 +1,5 @@
 ﻿class Utils {
-    dgSigla(dg1) {
-        var dg = '';
-
-        switch (dg1) {
-            case 90239://DIRECCIÓN GENERAL DE SERVICIOS GENERALES.
-                dg = 'DGSG';
-                break;
-            case 90014://Secretaría Ejecutiva de Administración
-                dg = 'SEA';
-                break;
-            case 90230://Coordinación de Administración Regional  
-                dg = 'CAR';
-                break;
-            case 90017://Dirección General de Inmuebles y Mantenimiento 
-                dg = 'DGIM';
-                break;
-            case 90240://Dirección General de Recursos Materiales
-                dg = 'DGRM';
-                break;
-            case 90015://Dirección General de Recursos Humanos
-                dg = 'DGRH';
-                break;
-            case 90228://Dirección General de Tecnologías de la Información
-                dg = 'DGTI';
-                break;
-            case 90202://Dirección General de Servicios al Personal
-                dg = 'DGSP';
-                break;
-            case 9471://Dirección de Innovación, Planeación y Desarrollo Institucional
-                dg = 'DGIPDI';
-                break;
-            case 90328://Direccion General de Programación, Presupuesto y Tesorería
-                dg = 'DGPPT';
-                break;
-            case 90231://Dirección General de Protección Civil y Salud en el Trabajo
-                dg = 'DGPCST';
-                break;
-            default:
-                dg = 'NA';
-                break;
-        }
-
-        return dg;
-    }
-    logout() {
-        localStorage.removeItem('user');
-    }
+    
 
     handleResponse(response) {
         if (response.status === 401) {
